3d09d333d9ac7cdf68eab8d9f0191635afda0ee9
root <root@devvm.localdomain>
2011-08-30 05:31:23 -0400

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = 'd4cfdfc333616e402c4088ba94d83005d69024b4bc547698147c90f3662c0369644aeb1cd445fcdd99dc088830aa9f49441a5f36c3f06879fd9455a7c2940632'
